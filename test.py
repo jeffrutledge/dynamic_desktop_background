d15c4e2399af007c3c7d9bc763c74f7bc55a786e
Jeffrey Rutledge <jeff_rutledge@icloud.com>
2016-12-31 21:38:54 -0800

    existing files

---
+++ test.py
@@ -0,0 +1,51 @@
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
+import subprocess
+import requests
+import json
+
+def get_location():
+    request_url = 'http://geoip.nekudo.com/api//en/short'
+    location_json = requests.get(request_url).json()
+
+    with open('location.json', 'w') as location_file:
+        json.dump(location_json, location_file)
+
+def load_location():
+    with open('location.json', 'r') as location_file:
+        location = json.load(location_file)['location']
+    return location
+
+def save_request(file_name, request_url):
+    request_json = requests.get(request_url).json()
+
+    print(request_json)
+
+    with open(file_name, 'w') as outfile:
+        json.dump(outfile, file_name)
+
+def load_request(file_name):
+    with open(file_name, 'r') as infile:
+        request_json = json.load(infile)
+
+    return request_json
+
+def get_weather_location(api_key):
+    request_url = ''
+    weather_request = requests.get()
+
+def update_desktop_bg(temp):
+    desktop_bg = Image.open('./graphics/meditating_person_light_white_bg.png')
+    draw = ImageDraw.Draw(desktop_bg)
+    font = ImageFont.truetype('/System/Library/Fonts/Avenir.ttc', 35)
+    text = u'{:2.0f}°F'.format(temp)
+    text_size = draw.textsize(text, font=font)
+    draw.text((640 - text_size[0] / 2, 310 - text_size[1] / 2), text, (255, 255, 255), font=font)
+    desktop_bg.save('desktop_bg.png')
+
+    # Forces Mac OS to update desktop background
+    subprocess.run(['/usr/bin/killall', 'Dock'])
+
+test = load_request('./autoip.json')
+print(test['current_observation']['temp_f'])
